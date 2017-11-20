4691fbbee9637eb97a0dfd0d5792592f2e0327f2
icepic <icepic@mapsource.fritz.box>
2017-11-20 01:35:29 +0100

    Working overload for function returning void

--- define.hpp
+++ define.hpp
@@ -11,31 +11,31 @@ namespace geil {
 namespace detail{
 
 template <typename R, typename Fn>
-auto define_function_helper_impl(Fn fn, std::tuple<R>, std::tuple<>) {
+auto define_function_helper_impl(Fn fn, pack<R>, pack<>) {
     static const Fn fn_ = fn;
     return []() -> SCM { return to_scm(fn_()); };
 }
 
-// template <typename R, typename Fn, typename T1>
-// auto define_function_helper_impl(Fn fn, std::tuple<void>, std::tuple<T1>)
-// {
-//     static const Fn fn_ = fn;
-//     return [](SCM a1) -> SCM {
-//         fn_(to_cpp<T1>(a1));
-//         return SCM_UNSPECIFIED;
-//     };
-// }
+template <typename Fn, typename T1>
+auto define_function_helper_impl(Fn fn, pack<void>, pack<T1>)
+{
+    static const Fn fn_ = fn;
+    return [](SCM a1) -> SCM {
+        fn_(to_cpp<T1>(a1));
+        return SCM_UNSPECIFIED;
+    };
+}
 
 template <typename Fn, typename... Args>
-auto define_function_helper(Fn fn, std::tuple<Args...>) {
+auto define_function_helper(Fn fn, pack<Args...>) {
     return define_function_helper_impl(
-        fn, std::tuple<std::result_of_t<Fn(Args...)>>{}, std::tuple<Args...>{});
+        fn, pack<std::result_of_t<Fn(Args...)>>{}, pack<Args...>{});
 }
 
 template <typename Fn>
 static void define_function(const std::string& name, Fn fn) {
-    using args_t = boost::callable_traits::args_t<Fn>;
-    constexpr auto args_size = std::tuple_size<args_t>::value;
+    using args_t = boost::callable_traits::args_t<Fn,pack>;
+    constexpr auto args_size = pack_size_v<args_t>;
 
     auto subr = (scm_t_subr) +define_function_helper(fn, args_t{});
     scm_c_define_gsubr(name.c_str(), args_size, 0, 0, subr);
